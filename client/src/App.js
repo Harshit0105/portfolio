5c6df2e809b1975dbc4e4b615907a7f2f149cc99
Harshit0105 <Harshit010501@gmail.com>
2021-09-16 10:32:10 +0530

    Bugs Solved

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Sidebar from "./Components/Sidebar";
 import styled from "styled-components";
 import HomePage from "./Pages/HomePage";
---
+++ client/src/scripts/home.js
@@ -0,0 +1,22 @@
+(function () {
+  // const select = (el, all = false) => {
+  //   el = el.trim();
+  //   if (all) {
+  //     return [...document.querySelectorAll(el)];
+  //   } else {
+  //     return document.querySelector(el);
+  //   }
+  // };
+  const typed = [...document.querySelectorAll(".typed")];
+  if (typed) {
+    let typed_strings = typed.getAttribute("data-typed-items");
+    typed_strings = typed_strings.split(",");
+    new Typed(".typed", {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+    });
+  }
+})();
